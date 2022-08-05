289bf933e65b5de7d8fecd1afcae4aee6eb22110
Andrii V <kidstonek@gmail.com>
2022-08-05 16:29:24 +0300

    First part = change place for timer

--- HW_05.py
+++ HW_05.py
@@ -13,11 +13,9 @@ def factorize(*number):
     return ans
 
 
-script_start = datetime.now()
-a, b, c, d = factorize(128, 255, 99999, 10651060)
-
-
 if __name__ == '__main__':
+    script_start = datetime.now()
+    a, b, c, d = factorize(128, 255, 99999, 10651060)
 
     assert a == [1, 2, 4, 8, 16, 32, 64, 128]
     assert b == [1, 3, 5, 15, 17, 51, 85, 255]
@@ -25,5 +23,5 @@ if __name__ == '__main__':
     assert d == [1, 2, 4, 5, 7, 10, 14, 20, 28, 35, 70, 140, 76079, 152158, 304316, 380395, 532553, 760790, 1065106,
                  1521580, 2130212, 2662765, 5325530, 10651060]
 
-script_end = datetime.now()
-print((script_end - script_start).total_seconds())
+    script_end = datetime.now()
+    print((script_end - script_start).total_seconds())
